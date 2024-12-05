c29a84e2c8a4de17619c5aea49d2a7facf3a6ef5
Isma Hasnawati <ismahasnawati758@gmail.com>
2024-12-05 22:02:03 +0700

    enrollment

---
+++ app/Http/Controllers/Admin/EnrollmentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Course;
+use App\Models\Enrollment;
+use App\Models\Instructor;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Yajra\DataTables\DataTables;
+
+class EnrollmentController extends Controller {}
